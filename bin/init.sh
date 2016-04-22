548325299be11c88eabe0f73e5772b2a45452481
Josh King <jking@chambana.net>
2016-04-22 01:24:50 -0400

    Add missing semicolons.

--- bin/init.sh
+++ bin/init.sh
@@ -8,10 +8,10 @@ CHECK_VAR AMAVIS_REINJECTION_PORT
 
 MSG "Configuring Amavis..."
 
-URI="'lmtp:[$(getent hosts $AMAVIS_REINJECTION_HOST  | awk '{ print $1 }')]:$AMAVIS_REINJECTION_PORT'"
+URI="'lmtp:[$(getent hosts $AMAVIS_REINJECTION_HOST | head -n1 | awk '{ print $1 }')]:$AMAVIS_REINJECTION_PORT'"
 
-sed -i -e "s/^\$notify_method\ *=.*/\$notify_method\ =\ $URI/" \
-  -e "s/^\$forward_method\ *=.*/\$forward_method\ =\ $URI/" \
+sed -i -e "s/^\$notify_method\ *=.*/\$notify_method\ =\ $URI;/" \
+  -e "s/^\$forward_method\ *=.*/\$forward_method\ =\ $URI;/" \
 	/etc/amavis/conf.d/50-user
 
 supervisord -c /etc/supervisor/supervisord.conf 
